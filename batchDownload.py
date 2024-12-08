8cf79f0b75a410023eab2277d1dda27d5c237a51
Jeffrey A. Turner <jeffturner7@gmail.com>
2024-12-08 12:47:58 -0500

    updates

--- batchDownload.py
+++ batchDownload.py
@@ -27,39 +27,51 @@ import os
 
 # List of sessions you'd like to download. They go to the Data folder in the 
 # current directory.
-sessionList = ["ae3b4aef-9a79-4c0f-8b7b-c4f51c1a78b4",
-"d7a1f78f-b2ae-4f36-9606-755f01106065",
-"1d23d144-5a61-4437-bf6d-c97e8ef31bf5",
-"096a206f-a7b8-4ec8-8646-8f93aea80ef3",
-"e307463b-e24e-49e9-b757-4f617980577e",
-"9788c25d-2665-4e19-9dd3-f480dcb9ccc7",
-"4a4b4092-d059-4c46-94f1-af4bda6c9d5b",
-"0170fed3-2d26-4b81-a91f-c111f949e337",
-"db4a8978-3e94-4609-867a-c3f668d2ee94",
-"2cd52db6-ab58-4e46-a5d4-0994b8919cd7",
-"58328c28-d582-4222-b647-a3495b5fa77b",
-"532e592f-4dc8-4b7f-9c2f-b1ec1bd401ef",
-"8987d69f-19f0-4e88-9023-bb405070d86a",
-"db5ad326-5812-4a29-81e0-ffc29fa8dbb1",
-"922048e9-45d1-4a64-a707-bd444341b0de",
-"a204f7d7-6bdf-4978-a63a-8e13fc7509bd",
-"8d5a0f2b-f7c4-4bce-8a71-c103e2254c43",
-"b086defa-69bf-44fc-b76f-8b33f58ff942",
-"4ea7a4ff-4cf2-43f8-8aba-c25b2a70377c",
-"0543f46e-1b94-46d2-98ca-d3836b6a8d06",
-"d5eb6b38-6e68-4686-8cd2-2efa0cd42356",
-"00875ec5-a122-42b1-9e25-ce3dcf1ba2d7",
-"78853c59-539d-43f0-90ad-5cda0318ec28",
-"2833c130-0461-40b1-931e-e380f5304550",
-"c2cdde75-3d3b-4878-a603-40fdb8db391d",
-"c666e950-8502-452b-a339-5eff7795e6d7",
-"18d32184-c6fb-42a8-ab98-c209ab50f4d5",
-"15b44950-fce7-435b-80f5-a1233d8b094f",
-"a9a1d84b-3f5a-4d9c-8aeb-3cbdfde3bf7a",
-"a2c6d875-3da5-4ac3-aa5c-a44802ae5643",
-"a83d868e-4838-4847-8e57-db9876714bb3",
-"df766d60-2602-4324-aac4-66712019e278"]
+sessionList = ["020a52a1-9f81-4397-919c-2fb51d28ae13",
+"7e9d7d1e-81ce-4594-84e6-442fc5600c4d",
+"faf935c7-d96e-4836-b5d8-084468b54893",
+"faf3ca0e-2b70-4473-aece-907361eead8b",
+"0b419b78-3f37-423c-b153-542b457ad2f9",
+"aff4f739-d474-42a1-ad8d-10c36b9f858a",
+"c452f5e2-511e-4b1c-bfa7-cdd5287eeac4",
+"888cdd7d-e667-4367-ba7f-8f07c2a7300c",
+"6656c1c9-bcff-4fbc-ae02-14b03c3b9d11",
+"91a5cdbf-68c8-4bb9-9882-814ac0ffc3ba",
+"4683fcf3-9095-4c40-90f3-d28d51e9ea2f",
+"d28f76e3-7c73-47f2-9ffa-4169eda7b332",
+"e33fa035-9c8b-4804-be95-bf2add896b33",
+"f6f38b0f-89d1-44e2-a5f7-775416f891e6",
+"9712a362-6d58-4b4b-a263-82a7f7e5ef53",
+"bba7d155-8bd0-4a38-a64f-385dc4cfd967",
+"82bf2539-de48-45e5-bba7-e9fb66c33e34",
+"7c8ac96c-b604-404a-9d8c-b3e20d66f744",
+"a7a9080c-2fc2-4cd1-974f-c2f0753135e7",
+"183c8653-143b-4265-b985-1d7e64b90697",
+"8d5fd2fa-4bd2-4fbe-bdc1-8ba7d3f7db81",
+"17c6ea2d-5066-4b36-a99d-82428f6b9bd2",
+"f475e82d-eb8b-4b53-8a7d-dde74ccd0595",
+"e4aee237-e494-4ffb-95db-a1a327c117e4",
+"24be6856-eb5c-4968-8227-6316dfe70e52",
+"dae00de9-319b-4d1c-b1ad-7ddf9d6ee9c9",
+"c2f65835-64dd-4e73-82b5-7aa6b4bc9207",
+"dd589f19-b42a-4173-aa5b-61b67606e44b",
+"bf03d7b2-aab5-46ba-9a6e-ac04e1841a43",
+"97c75f9b-afe1-499a-bee1-74656c5465b3",
+"f1c4ed2b-b69b-460a-bcd1-eaf24e8a2952",
+"0a9173cc-9ef5-4fb8-b0d1-5efb0891ea8f",
+"40fcea72-4fd7-47ba-9ffb-efc6b4f60cab",
+"34b4c05d-8ca3-4bd8-aaf7-f3df5988ed9b",
+"63c40bee-093c-4b76-96c0-ae21a8ed100f",
+"6f2699ed-e48c-4b74-8076-2f5d254bc901",
+"48acff60-52b8-4e02-8641-44470168a7fe",
+"e975d170-8b66-4cde-80eb-4c1645199f93",
+"f40efc12-02eb-4470-96cf-9177c80c004c",
+"2c9a92d5-22d3-4b41-ad17-f0b1e6464857"
 
+]
+
+#uncmotion
+#UNCmotion=1opencapAI
 
 # # alternatively, read list of session IDs from CSV column
 # from pathlib import Path
@@ -70,7 +82,7 @@ sessionList = ["ae3b4aef-9a79-4c0f-8b7b-c4f51c1a78b4",
 
              
 # base directory for downloads. Specify None if you want to go to os.path.join(os.getcwd(),'Data')
-downloadPath = os.path.join(os.getcwd(),'UCL2023')
+downloadPath = os.path.join(os.getcwd(),'Aim3')
 
 for session_id in sessionList:
     # If only interested in marker and OpenSim data, downladVideos=False will be faster
